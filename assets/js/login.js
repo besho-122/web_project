72f0fd6d239aa83f100b88824d8a8182cee983e5
besho-122 <m123456789@gmail.com>
2025-08-05 13:02:20 +0300

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,37 @@
+let isAtLeft = false;
+function toggleForm() {
+  const signupForm = document.querySelector('.signupForm');
+  const signinForm = document.querySelector('.signinForm');
+  const swiper = document.querySelector('.swiper');
+  const swiperImage = document.getElementById('swiperImage');
+  const toggleText = document.getElementById('toggleText');
 
-    const container = document.getElementById('container');
-    const toggleText = document.getElementById('toggleText');
+  if (isAtLeft) {
+    swiper.classList.remove('leftswiper');
+    swiper.classList.add('rightswiper');
+    signupForm.classList.add('signupvisible');
+    signupForm.classList.remove('signuphidden');
+    signinForm.classList.add('signinhidden');
+    signinForm.classList.remove('signinvisible');
+    fadeImage(swiperImage, "../assets/photos/discover2.jpg"); 
+    toggleText.textContent = "Already have an account? Sign In";
+  } else {
+    swiper.classList.remove('rightswiper');
+    swiper.classList.add('leftswiper');
+    signupForm.classList.add('signuphidden');
+    signupForm.classList.remove('signupvisible');
+    signinForm.classList.add('signinvisible');
+    signinForm.classList.remove('signinhidden');
+    fadeImage(swiperImage, "../assets/photos/discover1.jpeg");
+    toggleText.textContent = "Don't have an account? Sign Up";
+  }
 
-    function toggleForm() {
-      container.classList.toggle('active');
-      if (container.classList.contains('active')) {
-        toggleText.textContent = "Don't have an account? Sign Up";
-      } else {
-        toggleText.textContent = "Already have an account? Sign In";
-      }
-    }
+  isAtLeft = !isAtLeft;
+}
+
+function fadeImage(imgElement, newSrc) {
+  imgElement.classList.remove('fade');
+  void imgElement.offsetWidth;
+  imgElement.src = newSrc;
+  imgElement.classList.add('fade');
+}
